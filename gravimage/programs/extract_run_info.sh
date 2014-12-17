8a868de55ec483434481d4f65c263ab36c5d2506
PascalSteger <psteger@phys.ethz.ch>
2014-12-17 16:06:25 +0100

    format corrected

--- gravimage/programs/extract_run_info.sh
+++ gravimage/programs/extract_run_info.sh
@@ -10,7 +10,7 @@ do
     for case in $inv/?([1,2])[0-9]/
     do
         #echo  $case
-        var=$(ls $case/* |grep -E /201[0-9]|cut -d":" -f1)
+        var=$(ls $case* |grep -E /201[0-9]|cut -d":" -f1)
         count=1
         for timestamp in $var
         do
@@ -23,7 +23,7 @@ do
                 else
                     lines=$(wc -l $timestamp/ev.dat|cut -d" " -f1)
                     nbeta=$(grep "self.nbeta =" $timestamp/programs/gl_params.py | cut -d"=" -f2 | cut -d"#" -f1)
-                    pops=$(grep "self.pops =" $timestamp/programs/gl_params.py | cut -d"=" -f2 | cut -d"#" -f1)
+                    pops=$(grep "self.pops =" $timestamp/programs/gl_params.py | cut -d"=" -f2 | head -n1 | cut -d"#" -f1)
                     bins=$(grep "self.nipol =" $timestamp/programs/gl_params.py | cut -d"=" -f2 | cut -d"#" -f1)
 
                     echo -e $count"\t"$timestamp"\t"$lines"\t"$pops"\t"$bins"\t"$nbeta
