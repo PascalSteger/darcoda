removed batch plotting, introduced threads for plotting introduced pool for loglike calculation still bug for number of models stored, need to search for possibility to walk through all stored models

gravimage/programs/gravimage.py
@@ -65,7 +65,7 @@ def myloglike(cube, ndim, nparams):
     # TODO: with parallel version, need to append to CPU-based output name
     # we only store models after the initial Sigma burn-in
     if gp.chi2_Sig_converged <= 0:
-        tmp_profs.x0 = gp.xipol
+        tmp_profs.x0 = gp.xepol
         tmp_profs.xbins = np.hstack([gp.dat.binmin, gp.dat.binmax[-1]])
         with open(gp.files.outdir+'pc2.save', 'ab') as fi:
             pickle.dump(tmp_profs, fi)

gravimage/programs/plotting/gi_collection.py
@@ -8,6 +8,7 @@
 import numpy as np
 import numpy.random as npr
 import pdb
+from multiprocessing import Process
 
 import matplotlib.pyplot as plt
 plt.ioff()
@@ -44,7 +45,7 @@ def unit(prof):
 
 
 class ProfileCollection():
-    def __init__(self, pops, nipol):
+    def __init__(self, pops, nepol):
         self.chis = []
         self.goodchi = []  # will contain all chi^2 for models in self.subset
         self.goodprof = [] # will contain the corresponding profiles
@@ -53,18 +54,18 @@ class ProfileCollection():
         self.x0 = np.array([])
         self.binmin = np.array([])
         self.binmax = np.array([])
-        self.Mmin  = Profiles(pops, nipol)
-        self.M95lo = Profiles(pops, nipol)
-        self.M68lo = Profiles(pops, nipol)
-        self.Mmedi = Profiles(pops, nipol)
-        self.M68hi = Profiles(pops, nipol)
-        self.M95hi = Profiles(pops, nipol)
-        self.Mmax  = Profiles(pops, nipol)
+        self.Mmin  = Profiles(pops, nepol)
+        self.M95lo = Profiles(pops, nepol)
+        self.M68lo = Profiles(pops, nepol)
+        self.Mmedi = Profiles(pops, nepol)
+        self.M68hi = Profiles(pops, nepol)
+        self.M95hi = Profiles(pops, nepol)
+        self.Mmax  = Profiles(pops, nepol)
         self.analytic = Profiles(pops, 100)
-    ## \fn __init__(self, pops, nipol)
+    ## \fn __init__(self, pops, nepol)
     # constructor
     # @param pops number of populations
-    # @param nipol number of bins
+    # @param nepol number of bins
 
     def add(self, prof):
         if np.isnan(prof.chi2):
@@ -108,9 +109,9 @@ class ProfileCollection():
 
 
     def set_x0(self, x0, Binmin, Binmax):
-        self.x0 = x0
         self.binmin = Binmin
         self.binmax = Binmax
+        self.x0 = x0
         self.Mmin.x0  = x0
         self.M95lo.x0 = x0
         self.M68lo.x0 = x0
@@ -168,21 +169,28 @@ class ProfileCollection():
 
 
     def sort_profiles(self, gp):
-        self.sort_prof('rho', 0, gp)
-        self.sort_prof('M', 0, gp)
-        self.sort_prof('Sig', 0, gp)
-        self.sort_prof('nr', 0, gp)
-        self.sort_prof('nrnu', 0, gp)
-        self.sort_prof('nu', 0, gp)
+        pr=[]
+        pr.append(Process(target=self.sort_prof, args=('rho', 0, gp,)))
+        pr.append(Process(target=self.sort_prof, args=('M', 0, gp,)))
+        pr.append(Process(target=self.sort_prof, args=('Sig', 0, gp,)))
+        pr.append(Process(target=self.sort_prof, args=('nr', 0, gp,)))
+        pr.append(Process(target=self.sort_prof, args=('nrnu', 0, gp,)))
+        pr.append(Process(target=self.sort_prof, args=('nu', 0, gp,)))
         for pop in np.arange(1, gp.pops+1):
-            self.sort_prof('betastar', pop, gp)
-            self.sort_prof('beta', pop, gp)
-            self.sort_prof('nu', pop, gp)
-            self.sort_prof('nrnu', pop, gp)
-            self.sort_prof('Sig', pop, gp)
-            self.sort_prof('sig', pop, gp)
+            pr.append(Process(target=self.sort_prof, args=('betastar', pop, gp,)))
+            pr.append(Process(target=self.sort_prof, args=('beta', pop, gp,)))
+            pr.append(Process(target=self.sort_prof, args=('nu', pop, gp,)))
+            pr.append(Process(target=self.sort_prof, args=('nrnu', pop, gp,)))
+            pr.append(Process(target=self.sort_prof, args=('Sig', pop, gp,)))
+            pr.append(Process(target=self.sort_prof, args=('sig', pop, gp,)))
+        for i in pr:
+            i.start()
+        for i in pr:
+            i.join()
+        pdb.set_trace()
+        return
     ## \fn sort_profiles(self, gp)
-    # sort all profiles
+    # sort all profiles, in a parallel way
     # @param gp global parameters
 
 
@@ -253,7 +261,7 @@ class ProfileCollection():
     def write_prof(self, basename, prof, pop, gp):
         output = go.Output()
         uni = unit(prof)
-        output.add('radius (models) [pc]', self.Mmedi.x0)
+        output.add('radius (models) [pc]', gp.xepol)
         output.add('M 95% CL low ' + uni, self.M95lo.get_prof(prof, pop))
         output.add('M 68% CL low ' + uni, self.M68lo.get_prof(prof, pop))
         output.add('M median '     + uni, self.Mmedi.get_prof(prof, pop))
@@ -327,7 +335,7 @@ class ProfileCollection():
 
     def plot_data(self, ax, basename, prof, pop, gp):
         output = go.Output()
-        r0 = self.Mmedi.x0 # [pc]
+        r0 = gp.xipol # [pc]
         output.add('radius (data) [pc]', r0)
         if prof == 'Sig':
             # get 2D data here
@@ -457,15 +465,6 @@ class ProfileCollection():
     # @param ax axis object
     # @param gp global parameters
 
-
-    def fill_nice(self, ax, prof, pop, gp):
-        scaleHS = 1.0
-        if prof == 'nu':
-            scaleHS = gp.nu0pc[pop]
-        M95lo = self.M95lo.get_prof(prof, pop)*scaleHS
-        M68lo = self.M68lo.get_prof(prof, pop)*scaleHS
-        Mmedi = self.Mmedi.get_prof(prof, pop)*scaleHS
-
     def plot_full_distro(self, ax, prof, pop, gp):
         x = self.x0
         y = self.sort_prof(prof, pop, gp) # gives [Nmodels, Nbin] shape matrix
@@ -476,7 +475,7 @@ class ProfileCollection():
         H, xedges, yedges = np.histogram2d(x, y, bins=[xbins, ybins])
         fig, ax = plt.subplots(figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
 
-        ax.set_xlim([self.x[0]/2,self.x0[-1]*1.5])
+        ax.set_xlim([self.x[0],self.x0[-1]])
 
         extent = [min(xbins), max(xbins), min(ybins), max(ybins)]
         im = ax.imshow(H.T, extent=extent, aspect='auto', interpolation='nearest', cmap=plt.cm.binary) #plt.cm.Blues)
@@ -499,9 +498,11 @@ class ProfileCollection():
         M95lo = self.M95lo.get_prof(prof, pop)
         M68lo = self.M68lo.get_prof(prof, pop)
         Mmedi = self.Mmedi.get_prof(prof, pop)
-        r0    = self.Mmedi.x0
+        r0    = gp.xepol
         M68hi = self.M68hi.get_prof(prof, pop)
         M95hi = self.M95hi.get_prof(prof, pop)
+        print('lengths:', len(r0), len(M95lo), len(M95hi))
+        print('min max M95: ', min(M95lo), max(M95lo), prof)
         ax.fill_between(r0, M95lo, M95hi, color='black', alpha=0.2, lw=0.1)
         ax.plot(r0, M95lo, color='black', lw=0.4)
         ax.plot(r0, M95hi, color='black', lw=0.3)
@@ -514,7 +515,8 @@ class ProfileCollection():
                 ax.axvline(gp.Xscale[pop+1], color='blue', lw=0.5) # [pc]
         else:
             self.plot_Xscale_3D(ax, gp)
-        ax.set_xlim([r0[0]/2,r0[-1]*1.5])
+        ax.set_xlim([r0[0], r0[-1]])
+        ax.set_ylim([min(M68lo), max(M68hi)])
         return
     ## \fn fill_nice(self, ax, prof, pop, gp)
     # plot filled region for 1sigma and 2sigma confidence interval
@@ -581,46 +583,6 @@ class ProfileCollection():
     # @param prof string of profile to look at
     # @param pop population number
     # @param gp global parameters
-
-
-    def plot_from_ascii_chi(self, ax, chidata):
-        edges = chidata[:,0]
-        bins  = chidata[:,1]
-        ax.step(edges, bins, where='pre')
-    ## \fn plot_from_ascii_chi(self, ax, chidata)
-    # plot chi ascii files
-    # @param ax axis profile
-    # @param chidata data
-
-
-    def plot_from_ascii(self, ax, data, data_analytic, gp):
-        r0 = data[:,0]
-        M95lo = data[:,1]
-        M68lo = data[:,2]
-        Mmedi = data[:,3]
-        M68hi = data[:,4]
-        M95hi = data[:,5]
-        ax.fill_between(r0, M95lo, M95hi, color='black', alpha=0.2, lw=0.1)
-        ax.plot(r0, M95lo, color='black', lw=0.4)
-        ax.plot(r0, M95hi, color='black', lw=0.3)
-        ax.fill_between(r0, M68lo, M68hi, color='black', alpha=0.4, lw=0.1)
-        ax.plot(r0, M68lo, color='black', lw=0.4)
-        ax.plot(r0, M68hi, color='black', lw=0.3)
-        ax.plot(r0, Mmedi, 'r', lw=1)
-        for pop in range(gp.pops):
-            ax.axvline(gp.Xscale[pop+1], color='gray', lw=0.5) # [pc]
-
-        ranalytic = data_analytic[:,0]
-        analytic  = data_analytic[:,1]
-        ax.plot(ranalytic, analytic, lw=2, color='blue')
-        return
-    ## \fn plot_from_ascii(self, ax, data, data_analytic, gp)
-    # plot a single plot from previously calculated data
-    # @param ax axis object to plot into
-    # @param data array of (M95lo, M68lo, Mmedi, M68hi, M95hi) for the profile in question
-    # @param data_analytic analytic profile
-    # @param gp global parameters
-
 
     def __repr__(self):
         return "Profile Collection with "+str(len(self.profs))+" Profiles"

gravimage/programs/plotting/plot_profiles.py
@@ -15,6 +15,8 @@ import numpy.random as npr
 import time
 npr.seed(int(time.time())) # 1989 for random events that are reproducible
 from optparse import OptionParser
+from multiprocessing import Process
+
 import gi_helper as gh
 
 def prepare_output_folder(basename):
@@ -26,7 +28,6 @@ def prepare_output_folder(basename):
 ## \fn prepare_output_folder(basename)
 # create directory structure in output folder
 # @param basename string, data folder
-
 
 def read_scale(basename, gp):
     gp.Xscale = []
@@ -45,7 +46,6 @@ def read_scale(basename, gp):
 # read scale file, store into gp.*scale
 # @param basename string
 # @param gp
-
 
 def correct_E_error(filename):
     import os
@@ -80,13 +80,10 @@ def read_models(basename):
 
 def pcload_single_entries(basename, gp):
     import gi_collection as glc
-    pc = glc.ProfileCollection(gp.pops, gp.nipol)
+    pc = glc.ProfileCollection(gp.pops, gp.nepol)
     import re
     tmp = re.split('/DT', basename)[-1]
     path = str.join('/', re.split('/', tmp)[:-1])
-
-    rinfo = basename+'../../../run_info'
-
     # get number of iterations from run_info file
     import gi_base as gb
     bp = gb.get_basepath()
@@ -98,7 +95,6 @@ def pcload_single_entries(basename, gp):
                 runparams = line2
     fil.close()
     numofmodels = int(re.split('\t', runparams)[2])
-
     current = 0
     with open(basename+'pc2.save', 'rb') as fi:
         dum = pickle.load(fi) # dummy variable, was used to create file
@@ -145,33 +141,42 @@ def run(timestamp, basename, gp):
     pc.plot_profile(basename, 'chi2', 0, gp)
     # then select only the best models for plotting the profiles
     pc.cut_subset()
-    pc.set_x0(gp.xipol, Binmin*gp.Xscale[0], Binmax*gp.Xscale[0]) # [pc]
+    pc.set_x0(gp.xepol, Binmin*gp.Xscale[0], Binmax*gp.Xscale[0]) # [pc]
     if gp.investigate =='walk' or gp.investigate=='gaia':
         r0analytic = np.logspace(np.log10(1.),\
                                  np.log10(max(gp.xepol)), 100)
         pc.set_analytic(r0analytic, gp)
     pc.sort_profiles(gp)
     pc.write_all(basename, gp)
-    pc.plot_profile(basename, 'rho', 0, gp)
-    if gp.investigate == 'obs':
-        pc.plot_profile(basename, 'Sig', 0, gp)
-        pc.plot_profile(basename, 'nu', 0, gp)
-        pc.plot_profile(basename, 'nrnu', 0, gp)
-    pc.plot_profile(basename, 'nr', 0, gp)
-    if gp.geom == 'sphere':
-        pc.plot_profile(basename, 'M', 0, gp)
-    for pop in np.arange(1, gp.pops+1):
-        pc.plot_profile(basename, 'betastar', pop, gp)
-        pc.plot_profile(basename, 'beta', pop, gp)
-        pc.plot_profile(basename, 'Sig', pop, gp)
-        pc.plot_profile(basename, 'nu', pop, gp)
-        pc.plot_profile(basename, 'nrnu', pop, gp)
-        pc.plot_profile(basename, 'sig', pop, gp)
 
+    # start following plotting routines as threads, in parallel
+    pr=[]
+    pr.append(Process(target=pc.plot_profile, args=(basename, 'rho', 0, gp,)))
+    pr.append(Process(target=pc.plot_profile, args=(basename, 'nr', 0, gp)))
+    if gp.investigate == 'obs':
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'Sig', 0, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'nu', 0, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'nrnu', 0, gp,)))
+    if gp.geom == 'sphere':
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'M', 0, gp,)))
+    for pop in np.arange(1, gp.pops+1):
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'betastar', pop, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'beta', pop, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'Sig', pop, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'nu', pop, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'nrnu', pop, gp,)))
+        pr.append(Process(target=pc.plot_profile, args=(basename, 'sig', pop, gp,)))
+
+    for i in pr:
+        i.start()
+    for i in pr:
+        i.join()
     if gp.pops == 1:
-        os.system("cd "+basename+"/output/pdf/; pdfjam --outfile cat.pdf --nup 3x4 --no-landscape prof_nr_0.pdf prof_rho_0.pdf prof_M_0.pdf prof_nrnu_1.pdf prof_nu_1.pdf prof_Sig_1.pdf prof_betastar_1.pdf prof_beta_1.pdf prof_sig_1.pdf ../prof_chi2_0.pdf; evince cat.pdf &")
+        os.system("cd "+basename+"/output/pdf/; pdfjam --outfile cat.pdf --nup 3x4 --no-landscape prof_nr_0.pdf prof_rho_0.pdf prof_M_0.pdf prof_nrnu_1.pdf prof_nu_1.pdf prof_Sig_1.pdf prof_betastar_1.pdf prof_beta_1.pdf prof_sig_1.pdf ../prof_chi2_0.pdf")
+        #; evince cat.pdf &")
     elif gp.pops == 2:
-         os.system("cd "+basename+"/output/pdf/; pdfjam --outfile cat.pdf --nup 3x5 --no-landscape prof_nr_0.pdf prof_rho_0.pdf prof_M_0.pdf prof_nrnu_1.pdf prof_nu_1.pdf prof_Sig_1.pdf prof_betastar_1.pdf prof_beta_1.pdf prof_sig_1.pdf prof_nrnu_2.pdf prof_nu_2.pdf prof_Sig_2.pdf prof_betastar_2.pdf prof_beta_2.pdf prof_sig_2.pdf; evince cat.pdf &")
+         os.system("cd "+basename+"/output/pdf/; pdfjam --outfile cat.pdf --nup 3x5 --no-landscape prof_nr_0.pdf prof_rho_0.pdf prof_M_0.pdf prof_nrnu_1.pdf prof_nu_1.pdf prof_Sig_1.pdf prof_betastar_1.pdf prof_beta_1.pdf prof_sig_1.pdf prof_nrnu_2.pdf prof_nu_2.pdf prof_Sig_2.pdf prof_betastar_2.pdf prof_beta_2.pdf prof_sig_2.pdf")
+         # ;evince cat.pdf &")
 ## \fn run(timestamp, basename, gp)
 # call all model read-in, and profile-plotting routines
 # @param timestamp string

gravimage/programs/sphere/gi_chi.py
@@ -6,9 +6,6 @@
 
 # (c) GPL v3 ETHZ 2014 Pascal Steger, psteger@phys.ethz.ch
 
-import types
-# TODO find missing modules
-
 import pdb
 import numpy as np
 import gi_helper as gh
@@ -37,7 +34,7 @@ def calc_chi2(profs, gp):
     for pop in np.arange(1, gp.pops+1): # [1, 2, ... , pops]
         Sigdat   = gp.dat.Sig[pop]      # [Munit/pc^2]
         Sigerr   = gp.dat.Sigerr[pop]   # [Munit/pc^2]
-        Sigmodel = profs.get_prof('Sig', pop)
+        Sigmodel = profs.get_prof('Sig', pop)[gp.nexp:-gp.nexp]
         chi2_Sig  = chi2red(Sigmodel, Sigdat, Sigerr, gp.nipol) # [1]
         chi2 += chi2_Sig                 # [1]
         gh.LOG(2, ' chi2_Sig   = ', chi2_Sig)
@@ -45,7 +42,7 @@ def calc_chi2(profs, gp):
         # use the following only if chi2_nu_converged used rather than Sig_converged
         #nudat   = gp.dat.nu[pop]      # [Munit/pc^2]
         #nuerr   = gp.dat.nuerr[pop]   # [Munit/pc^2]
-        #numodel = profs.get_prof('nu', pop)
+        #numodel = profs.get_prof('nu', pop)[gp.nexp:-gp.nexp]
         #chi2_nu  = chi2red(numodel, nudat, nuerr, gp.nipol) # [1]
         #chi2 += chi2_nu                 # [1]
         #gh.LOG(1, ' chi2_nu   = ', chi2_nu)
@@ -55,7 +52,8 @@ def calc_chi2(profs, gp):
 
         sigdat  = gp.dat.sig[pop]    # [km/s]
         sigerr  = gp.dat.sigerr[pop]    # [km/s]
-        chi2_sig = chi2red(profs.get_prof('sig', pop), sigdat, sigerr, gp.nipol) # [1]
+        smodel = profs.get_prof('sig', pop)[gp.nexp:-gp.nexp]
+        chi2_sig = chi2red(smodel, sigdat, sigerr, gp.nipol) # [1]
         chi2 += chi2_sig                # [1]
         gh.LOG(2, '  chi2_sig  = ', chi2_sig)
         if gp.usekappa:

gravimage/programs/sphere/gi_class_cube.py
@@ -93,7 +93,6 @@ def map_nr(params, prof, pop, gp):
     # get offset and n(r) profiles, calculate rho
     rhoscale = gp.rhohalf
     width = gp.log10rhospread
-    innerslope = gp.innerslope
     nrscale = gp.nrtol #/(max(np.log(gp.xipol))-min(np.log(gp.xipol)))
     monotonic = gp.monotonic
 
@@ -104,7 +103,7 @@ def map_nr(params, prof, pop, gp):
     rhohalf = 10**((params[0]-0.5)*2.*width+np.log10(rhoscale))
     # nr(r=0) is = rho slope for approaching r=0 asymptotically, given directly
     # should be smaller than -3 to exclude infinite enclosed mass
-    nrasym0 = params[1]*innerslope
+    nrasym0 = params[1]*gp.innerslope
     # work directly with the dn(r)/dlog(r) parameters here
     dnrdlrparams = params[1:]
 
@@ -157,9 +156,7 @@ def map_nr_tracers(params, pop, gp):
     # get offset and n(r) profiles, calculate rho
     rhoscale = gp.dat.nuhalf[pop]
     width = gp.log10nuspread
-    innerslope = gp.innerslope
     nrscale = gp.nrtol_nu/(max(np.log(gp.xipol))-min(np.log(gp.xipol)))
-    monotonic = gp.monotonic_nu
     # first parameter gives half-light radius value of rho directly
     # use [0,1]**3 to increase probability of sampling close to 0
     # fix value with tracer densities,
@@ -168,14 +165,14 @@ def map_nr_tracers(params, pop, gp):
     # 10** is correct
     # nr(r=0) is = rho slope for approaching r=0 asymptotically, given directly
     # should be smaller than -3 to exclude infinite enclosed mass
-    nrasym0 = params[1]**2*innerslope # **2 tweaks it toward lower values while still keeping general
+    nrasym0 = params[1]**2*gp.innerslope # **2 tweaks it toward lower values while still keeping general
     # work directly with the dn(r)/dlog(r) parameters here
     dnrdlrparams = params[1:]
 
     for k in range(0, gp.nepol):
         deltalogr = (np.log(gp.xepol[k-1])-np.log(gp.xepol[k-2]))
         # construct n(r_k+1) from n(r_k)+dn/dlogr*Delta log r, integrated
-        if monotonic:
+        if gp.monotonic_nu:
             # only increase n(r), use pa[i]>=0 directly
             nr[k] = nr[k-1] + dnrdlrparams[k] * nrscale/2. * deltalogr
         else:

gravimage/programs/sphere/gi_class_profiles.py
@@ -11,28 +11,29 @@ import numpy as np
 import gi_helper as gh
 
 class Profiles:
-    def __init__(self, pops, nipol):
+    def __init__(self, pops, nepol):
         self.pops = pops
-        self.nipol= nipol
-        self.x0   = np.zeros(nipol)
-        self.xbins = np.zeros(nipol)
+        self.nepol= nepol
+        self.x0   = np.zeros(nepol)
+        self.xbins = np.zeros(nepol)
         self.chi2 = 0.0
-        self.rho  = np.zeros(nipol)
-        self.nr   = np.zeros(nipol)
-        self.M    = np.zeros(nipol)
-        self.betastar = np.zeros((pops+1)*nipol)
-        self.beta = np.zeros((pops+1)*nipol) # (pops+1) for overall (rho*), 1, 2, ...
-        self.nu   = np.zeros((pops+1)*nipol) # dito
-        self.nrnu = np.zeros((pops+1)*nipol)
-        self.Sig  = np.zeros((pops+1)*nipol)
-        self.sig  = np.zeros((pops+1)*nipol)
-        self.kap  = np.zeros((pops+1)*nipol)
+        self.rho  = np.zeros(nepol)
+        self.nr   = np.zeros(nepol)
+        self.M    = np.zeros(nepol)
+        self.MtoL = -1.0
+        self.betastar = np.zeros((pops+1)*nepol)
+        self.beta = np.zeros((pops+1)*nepol) # (pops+1) for overall (rho*), 1, 2, ...
+        self.nu   = np.zeros((pops+1)*nepol) # dito
+        self.nrnu = np.zeros((pops+1)*nepol)
+        self.Sig  = np.zeros((pops+1)*nepol)
+        self.sig  = np.zeros((pops+1)*nepol)
+        self.kap  = np.zeros((pops+1)*nepol)
         self.zetaa = 0.
         self.zetab = 0.
-    ## \fn __init__(self, pops, nipol)
+    ## \fn __init__(self, pops, nepol)
     # constructor
     # @param pops number of populations
-    # @param nipol number of radial bins
+    # @param nepol number of radial bins
 
     def set_prof(self, prof, vec, pop, gp):
         gh.sanitize_vector(vec, len(self.x0), -200, 1e30, gp.debug)
@@ -43,19 +44,19 @@ class Profiles:
         elif prof == 'M':
             self.M = vec
         elif prof == 'nu':
-            self.nu[pop*self.nipol:(pop+1)*self.nipol] = vec # vec has nrho-3 entries
+            self.nu[pop*self.nepol:(pop+1)*self.nepol] = vec # vec has nrho-3 entries
         elif prof == 'nrnu':
-            self.nrnu[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.nrnu[pop*self.nepol:(pop+1)*self.nepol] = vec
         elif prof == 'betastar':
-            self.betastar[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.betastar[pop*self.nepol:(pop+1)*self.nepol] = vec
         elif prof == 'beta':
-            self.beta[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.beta[pop*self.nepol:(pop+1)*self.nepol] = vec
         elif prof == 'Sig':
-            self.Sig[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.Sig[pop*self.nepol:(pop+1)*self.nepol] = vec
         elif prof == 'sig':
-            self.sig[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.sig[pop*self.nepol:(pop+1)*self.nepol] = vec
         elif prof == 'kap':
-            self.kap[pop*self.nipol:(pop+1)*self.nipol] = vec
+            self.kap[pop*self.nepol:(pop+1)*self.nepol] = vec
         else:
             raise Exception('unknown profile to be set in gi_class_profiles.set_prof')
     ## \fn set_prof(self, prof, vec, pop, gp)
@@ -65,6 +66,12 @@ class Profiles:
     # @param pop population, if applicable
     # @param gp global parameters
 
+    def set_MtoL(self, MtoL):
+        self.MtoL = MtoL
+        return
+    ## \fn set_MtoL(self, MtoL)
+    # store mass-to-light ratio
+    # @param MtoL float
 
     def set_zeta(self, zetaa, zetab, pop):
         self.zetaa = zetaa
@@ -84,19 +91,19 @@ class Profiles:
         elif prof == 'M':
             return self.M
         elif prof == 'Sig':
-            return self.Sig[pop*self.nipol:(pop+1)*self.nipol]
+            return self.Sig[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'nu':
-            return self.nu[pop*self.nipol:(pop+1)*self.nipol]
+            return self.nu[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'nrnu':
-            return self.nrnu[pop*self.nipol:(pop+1)*self.nipol]
+            return self.nrnu[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'betastar':
-            return self.betastar[pop*self.nipol:(pop+1)*self.nipol]
+            return self.betastar[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'beta':
-            return self.beta[pop*self.nipol:(pop+1)*self.nipol]
+            return self.beta[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'sig':
-            return self.sig[pop*self.nipol:(pop+1)*self.nipol]
+            return self.sig[pop*self.nepol:(pop+1)*self.nepol]
         elif prof == 'kap':
-            return self.kap[pop*self.nipol:(pop+1)*self.nipol]
+            return self.kap[pop*self.nepol:(pop+1)*self.nepol]
         else:
             raise Exception('specify prof!')
     ## \fn get_prof(self, prof, pop)
@@ -113,7 +120,7 @@ class Profiles:
 
 
     def __repr__(self):
-        return "Profiles: "+str(self.pops)+" populations, "+str(self.nipol)+" nipol, "+" chi2 = "+str(self.chi2)
+        return "Profiles: "+str(self.pops)+" populations, "+str(self.nepol)+" nepol, "+" chi2 = "+str(self.chi2)
     ## \fn __repr__(self)
     # string representation for ipython
 

gravimage/programs/sphere/gi_params.py
@@ -49,7 +49,7 @@ class Params():
             self.case = case
             #os.system('sed -i "s/case = 1/case = '+str(case)+'/"')
         else:
-            self.case = 4 # gaia models (1..8) Walker (0..2,4,5; use 1, 2)
+            self.case = 2 # gaia models (1..8) Walker (0..2,4,5; use 1, 2)
                           # triax (1-4:core, 5-8:cusp), obs (0:for, 1: car, scl, sex)
         print(' case : ', self.case)
         self.pops = 1 # number of stellar tracer populations
@@ -137,7 +137,7 @@ class Params():
         self.rlimnr_nu = 1    # same for nrnu
         self.innerslope = 2.999
         self.maxnuslope = 5      # same for nrnu
-        self.nrtol  = 1 # scale of change of dn/dr
+        self.nrtol  = 0.5 # scale of change of dn/dr
         self.nrtol_nu = self.maxnuslope*2 # same for nu
         self.nupar_min = np.zeros(self.nrho)  # ranges to be sampled
         self.nupar_max = np.ones(self.nrho)*self.nrtol_nu
@@ -147,7 +147,7 @@ class Params():
         self.MtoLmin = 0.8
         self.MtoLmax = 3.
         self.monotonic = False   # monotonicity prior on n(x) for rho(x)
-        self.monotonic_nu = True # monotonicity prior on n(x) for nu(x)
+        self.monotonic_nu = False # monotonicity prior on n(x) for nu(x)
 
         # integration options
         # ----------------------------------------------------------------------