6971f99022a3b7a6cd186411ad3fec7e2405fb6a
PascalSteger <psteger@phys.ethz.ch>
2015-01-20 07:51:05 +0100

    revert gravimage.py back to working sphere version

--- gravimage/programs/gravimage.py
+++ gravimage/programs/gravimage.py
@@ -1,26 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env ipython3
 
 ##
 # @file
-# pymultinest run of gravimage integrals needs pymultinest from
-# http://johannesbuchner.github.io/PyMultiNest/
-# http://johannesbuchner.github.io/PyMultiNest/install.html#install-on-linux
 # needs Multinest from https://github.com/JohannesBuchner/MultiNest
 
-# TODO: run with mpirun -np <N> gravimage.py
-# where <N> is an integer <= number of processors
-# (c) 2013 ETHZ Pascal Steger, psteger@phys.ethz.ch
+# Copyright 2015 GPLv3 ETHZ, Pascal Steger, psteger@phys.ethz.ch
+
+# get latest version of PyMultiNest from
+# svn checkout http://lisasolve.googlecode.com/svn/trunk/ lisasolve-read-only
+
 
 ### imports
-# from __future__ import absolute_import, unicode_literals, print_function
-#from mpi4py import MPI
 import subprocess
-import numpy as np
 import pymultinest
 import pickle
-import gi_helper as gh
+import numpy as np
 import pdb
-import numpy.random as npr
+from multiprocessing import Pool
+
 # increment NICEness of process by 1, CPU usage shall not block others
 # import os
 # os.nice(1)
@@ -29,7 +26,7 @@ import numpy.random as npr
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-i", "--investigation", dest="investigation",
-                      default="", help="investigation to run: gaia, walk, hern, triax, discmock, simplenu")
+                      default="", help="investigation to run: gaia, walk, hern, triax, discmock")
 parser.add_option("-c", "--case", dest="case",
                       default=-1, help="case: 1, 2, ..")
 (options, args) = parser.parse_args()
@@ -48,13 +45,10 @@ def show(filepath):
 # filepath filename with full path
 
 def myprior(cube, ndim, nparams):
+    #ndim, nparams
     mycube = Cube(gp)
     mycube.copy(cube)
-    try:
-        cube = mycube.convert_to_parameter_space(gp)
-    except Exception:
-        gh.LOG(1, 'parameters not fulfilling prior requirements')
-
+    cube = mycube.convert_to_parameter_space(gp)
     return
 ## \fn myprior(cube, ndim, nparams) priors
 # @param cube [0,1]^ndim cube, array of dimension ndim
@@ -63,19 +57,13 @@ def myprior(cube, ndim, nparams):
 # stored with actual parameters
 
 def myloglike(cube, ndim, nparams):
-    #pdb.set_trace()
-    #if min(cube) == -9999:  # parameters not fulfilling prior requirements,
-    #    return -1e300       #      return very large chi2 #HS SEGFAULT CHECK THIS AGAIN
-    try:
-        tmp_profs = geom_loglike(cube, ndim, nparams, gp)
-    except ValueError:
-        return -1e100  # SS: think about sign...
+    tmp_profs = geom_loglike(cube, ndim, nparams, gp)
     # store tmp_prof by appending it to pc2.save
     # TODO: with parallel version, need to append to CPU-based output name
     # we only store models after the initial Sigma burn-in
-    if gp.chi2_nu_converged and npr.random()<0.001:#save only 0.1% of models
-        tmp_profs.x0 = gp.z_bincenters
-        tmp_profs.xbins = np.hstack([gp.z_binmins, gp.z_binmaxs[-1]])
+    if gp.chi2_Sig_converged <= 0:
+        tmp_profs.x0 = gp.xepol
+        tmp_profs.xbins = np.hstack([gp.dat.binmin, gp.dat.binmax[-1]])
         with open(gp.files.outdir+'pc2.save', 'ab') as fi:
             pickle.dump(tmp_profs, fi)
             # convention: use chi^2 directly, not log likelihood
@@ -93,7 +81,9 @@ def prepare_data(gp):
         if gp.getnewpos:
             gf.read_data(gp)
         gf.bin_data(gp)
-    gf.get_binned_data_noscale(gp)    #H Silverwood 20/11/14
+    if gp.getSigdata:
+        gf.read_Sigdata(gp)
+    gf.get_binned_data(gp)
     gp.files.populate_output_dir(gp)
     gf.get_rhohalfs(gp)
 ## \fn prepare_data(gp)
@@ -101,47 +91,47 @@ def prepare_data(gp):
 # @param gp global parameters
 
 def run(gp):
-    pymultinest.run(myloglike,   myprior,
-                    gp.ndim, n_params = gp.ndim+1, # None beforehands
-                    n_clustering_params = gp.ndim,# separate modes on
-                                                  # the rho parameters
-                                                  # only: gp.nrho
-                    wrapped_params = [ gp.ntracer_pops, gp.nbins, gp.nrhonu], # do
-                                                                     #not
-                                                                     #wrap-around
-                                                                     #parameters
-                    importance_nested_sampling = True, # INS enabled
-                    multimodal = True,           # separate modes
-                    const_efficiency_mode = True, # use const sampling efficiency
-                    n_live_points = gp.nlive,
-                    evidence_tolerance = 0.0, # set to 0 to keep
-                                              # algorithm working
-                                              # indefinitely
-                    sampling_efficiency = 0.5,
-                    n_iter_before_update = 100, # output after this many iterations
-                    null_log_evidence = -1e100,
-                    max_modes = gp.nlive,   # preallocation of modes:
-                                            #max. = number of live
-                                            #points
-                    mode_tolerance = -1.e100,   # mode tolerance in the
-                                               #case where no special
-                                               #value exists: highly
-                                               #negative
-                    outputfiles_basename = gp.files.outdir,
-                    seed = -1,
-                    verbose = True,
-                    resume = False,
-                    context = 0,
-                    write_output = True,
-                    log_zero = -1e500,    # points with log likelihood
-                                          #< log_zero will be
-                                          #neglected
-                    max_iter = 0,         # set to 0 for never
-                                          #reaching max_iter (no
-                                          #stopping criterium based on
-                                          #number of iterations)
-                    init_MPI = False,     # use MPI
-                    dump_callback = None)
+    pymultinest.run(myloglike,
+                  myprior,
+                  gp.ndim,
+                  n_params = gp.ndim+1,
+                  n_clustering_params = gp.ndim,# separate modes on
+                  # the rho parameters
+                  # only: gp.nrho
+                  wrapped_params = [ gp.pops, gp.nipol, gp.nrho],
+                  importance_nested_sampling = True, # INS enabled
+                  multimodal = True,           # separate modes
+                  const_efficiency_mode = True, # use const sampling efficiency
+                  n_live_points = gp.nlive,
+                  evidence_tolerance = 0.0, # set to 0 to keep
+                  # algorithm working
+                  # indefinitely
+                  sampling_efficiency = 0.25, # 0.05 according to
+                                              # MultiNest README for >
+                                              # 30 params
+                  n_iter_before_update = 5, # output after this
+                                                # many iterations
+                  null_log_evidence = -1e100,
+                  max_modes = gp.nlive,   # preallocation of modes:
+                  #max. = number of live points
+                  mode_tolerance = -1.e100,   # mode tolerance in the
+                                              # case where no special
+                                              # value exists: highly
+                                              # negative
+                  outputfiles_basename = gp.files.outdir,
+                  seed = -1,
+                  verbose = True,
+                  resume = gp.restart,   # TODO check
+                  context = 0,
+                  write_output = True,
+                  log_zero = -1e500,    # points with log likelihood
+                  #< log_zero will be
+                  #neglected
+                  max_iter = 0,         # set to 0 for never reaching max_iter (no
+                  #stopping criterium based on
+                  #number of iterations)
+                  init_MPI = False,     # use MPI
+                  dump_callback = None)
 
 if __name__=="__main__":
     global Cube, geom_loglike
@@ -155,7 +145,7 @@ if __name__=="__main__":
     # import sys
     # sys.stdout.write(hwmess % (myrank, nprocs, procnm))
 
-    # TODO: wait for prepare_data to finish
+    # TODO MPI: wait for prepare_data to finish
     # if MPI.COMM_WORLD.Get_rank() == 0:
     #     # TODO: wrong: have 0 take part in sampling as well
     prepare_data(gp) # run once
