364dfa4f97453beed4cff774a4634aa9df019b6f
PascalSteger <psteger@phys.ethz.ch>
2015-01-27 16:33:36 +0100

    update before change to get_sphere_*

--- gravimage/programs/gi_params.py
+++ gravimage/programs/gi_params.py
@@ -46,7 +46,7 @@ class Params():
             self.case = case
             #os.system('sed -i "s/case = 1/case = '+str(case)+'/"')
         else:
-            self.case = 2 # gaia models (1..8) Walker (0..2,4,5; use 1, 2)
+            self.case = 3 # gaia models (1..8) Walker (0..2,4,5; use 1, 2)
                           # triax (1-4:core, 5-8:cusp), obs (0:for, 1: car, scl, sex)
         print(' case : ', self.case)
         self.pops = 1 # number of stellar tracer populations
@@ -78,7 +78,7 @@ class Params():
         self.chi2_switch = 10
         self.chi2_Sig_converged = 1000 #how many times to be below that threshold?
         # Set number of terms for enclosedmass+tracer+anisotropy bins = model parameters:
-        self.nipol = 5   # IF CHANGED => set getnewdata = True to run data readout again
+        self.nipol = 12   # IF CHANGED => set getnewdata = True to run data readout again
         self.nexp  = 3    # more fudge parameters at r<rmin and r>rmax
         self.nepol = self.nipol + 2*self.nexp     # number of parameters for
                                                 # direct mapping of nu(r)
@@ -122,8 +122,8 @@ class Params():
         self.nrtol_nu = self.maxnuslope*2 # scale of change for nr_nu
         self.nupar_min = np.zeros(self.nrho)  # ranges to be sampled
         self.nupar_max = np.ones(self.nrho)*self.nrtol_nu
-        self.beta00prior = False  # beta(r=0)=0
-        self.minbetastar = -0.99  # clipping for beta*, default: -0.99
+        self.beta00prior = True  # beta(r=0)=0
+        self.minbetastar = -0.  # clipping for beta*, default: -0.99
         self.maxbetastar = 0.99  # clipping for beta*, default:  1.00
         self.MtoLmin = 0.8
         self.MtoLmax = 3.
