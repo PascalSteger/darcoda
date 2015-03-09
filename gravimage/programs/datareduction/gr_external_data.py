Fixed sigz calculation, added direct rho & nu scanning

gravimage/programs/datareduction/gr_external_data.py
@@ -92,7 +92,7 @@ def run(gp):
     # Use MC to estimate errors on nu
     if gp.investigate == 'simplenu':
         z_sampler = ErSamp_gauss_linear_w_z
-    nu_err_meas = gmcer.mc_nu_error(z_sampler, 100, binmin, binmax, bincentermed)
+    nu_err_meas = gmcer.mc_nu_error(z_sampler, gp.mc_err_N_iters, binmin, binmax, bincentermed)
 
     #Combine nu errors in quadrature TODO check that this is the right way to add errors
     nu_err_tot = np.sqrt(nu_err_pois**2 + nu_err_meas**2)
@@ -107,6 +107,10 @@ def run(gp):
     #Set central nu prior range
     gp.nu_C_max = 2.*Ntr/(binmax[0]-binmin[0])
     gp.nu_C_min = 0.1*Ntr/(binmax[-1]-binmin[0])
+
+    #Temp:
+    gp.nu_C_max = 3.E4
+    gp.nu_C_min = 1.E3
 
     import gr_params #WHAT DOES THIS DO.
     gpr = gr_params.Params(gp)

gravimage/programs/disc/gl_class_cube.py
@@ -19,6 +19,7 @@
 import pdb
 import numpy as np
 import gl_helper as gh
+import scipy.special
 
 def map_tilt_slope(vec, gp):
     T = np.zeros(len(vec))
@@ -209,6 +210,45 @@ def map_MtoL(pa, gp):
 # @param pa scalar
 # @param gp global parameters holding MtoL{min,max}
 
+def map_rhonu(params, prof, pop, gp):
+    if prof == 'rho':
+        rhonu_C_max = gp.rho_C_max
+        rhonu_C_min = gp.rho_C_min
+        monotonic = gp.monotonic_rho
+        prior_type = gp.prior_type_rho
+    elif prof == 'nu':
+        rhonu_C_max = gp.nu_C_max
+        rhonu_C_min = gp.nu_C_min
+        monotonic = gp.monotonic_nu
+        prior_type = gp.prior_type_nu
+
+    rhonu_C = rhonu_C_min + (rhonu_C_max-rhonu_C_min)*params[0]
+
+    # Pick rho/nu values between rhonu_C_max and rhonu_C_min, or if monotonic
+    # pick between rhonu_i_m1 and rhonu_C_min
+    rhonu_vector = []
+    rhonu_i_m1 = rhonu_C
+    for jter in range(1, gp.nbins+1):
+        if monotonic:
+            rhonu_C_max = rhonu_i_m1
+
+        if prior_type == 'linear':
+            rhonu_value = rhonu_C_min + (rhonu_C_max-rhonu_C_min)*params[jter]
+        elif prior_type == 'log':
+            rhonu_value = np.log10(rhonu_C_min) + (np.log10(rhonu_C_max)-np.log10(rhonu_C_min))*params[jter]
+            rhonu_value = 10**rhonu_value
+        elif prior_type == 'gaussian':
+            sigma_rhonu = (rhonu_C_max - rhonu_C_min)/4.
+            rhonu_value = rhonu_i_m1 + np.sqrt(2) * sigma_rhonu * scipy.special.erfinv(2*(params[jter]-0.5))
+            rhonu_value = max(rhonu_C_min, rhonu_value)
+            rhonu_value = min(rhonu_C_max, rhonu_value)
+
+        rhonu_vector.append(rhonu_value)
+        rhonu_i_m1 = rhonu_value
+
+    return np.hstack([rhonu_C, rhonu_vector])
+
+
 
 class Cube:
     def __init__ (self, gp):
@@ -234,7 +274,11 @@ class Cube:
 
         #Dark Matter mass profile parameters: rho_C, kz_C, kz_vector
         offstep = gp.nrhonu + 1
-        tmp_DM = map_kr(pc[off:off+offstep], 'rho', 0, gp)
+        if gp.scan_rhonu_space:
+            tmp_DM = map_rhonu(pc[off:off+offstep], 'rho', 0, gp)
+        else:
+            tmp_DM = map_kr(pc[off:off+offstep], 'rho', 0, gp)
+
         for i in range(offstep):
             pc[off+i] = tmp_DM[i]
         off += offstep
@@ -254,7 +298,11 @@ class Cube:
         #Tracer profile parameters: nu_C, kz_nu_C, kz_nu_vector # kz_nu_LS
         for tracer_pop in range(0, gp.ntracer_pops):
             offstep = gp.nrhonu + 1
-            tmp_tracer = map_kr(pc[off:off+offstep], 'nu', tracer_pop, gp)
+            if gp.scan_rhonu_space:
+                tmp_tracer = map_rhonu(pc[off:off+offstep], 'nu', tracer_pop, gp)
+            else:
+                tmp_tracer = map_kr(pc[off:off+offstep], 'nu', tracer_pop, gp)
+
             for i in range(offstep):
                 pc[off+i] = tmp_tracer[i]
             off += offstep

gravimage/programs/disc/gl_loglike.py
@@ -12,7 +12,8 @@ from gl_priors import check_bprior, check_tilt
 from gl_chi import calc_chi2
 import gl_physics as phys
 from pylab import *
-ion()
+#ion()
+import time
 
 def geom_loglike(cube, ndim, nparams, gp):
     tmp_profs = Profiles(gp.ntracer_pops, gp.nbins)#, gp.nbaryon_pops, gp.nbaryon_params)#, gp.nrhonu, )
@@ -24,15 +25,20 @@ def geom_loglike(cube, ndim, nparams, gp):
     off += offstep
 
     #Dark Matter rho parameters (rho_C, kz_C, kz_vector)
-    offstep = gp.nrhonu + 1
-    rho_DM_params = np.array(cube[off:off+offstep])
-    rho_DM_C = rho_DM_params[0] #rho_C
-    kz_rho_DM_allz = rho_DM_params[1:] #kz for rho across all z points [0, bin_centres]
-    tmp_rho_DM_allz = phys.rho(gp.z_all_pts, kz_rho_DM_allz, rho_DM_C) #outputs rho across all points
-    tmp_rho_total_allz = tmp_rho_DM_allz*1.0 # Add DM to total mass density
 
-    tmp_profs.kz_rho_DM_C = kz_rho_DM_allz[0]
-    tmp_profs.set_prof('kz_rho_DM_vec', kz_rho_DM_allz[1:], 0, gp)
+    offstep = gp.nrhonu + 1
+    if gp.scan_rhonu_space:
+        tmp_rho_DM_allz = np.array(cube[off:off+offstep])
+    else:
+        rho_DM_params = np.array(cube[off:off+offstep])
+        rho_DM_C = rho_DM_params[0] #rho_C
+        kz_rho_DM_allz = rho_DM_params[1:] #kz for rho across all z points [0, bin_centres]
+        tmp_rho_DM_allz = phys.rho(gp.z_all_pts, kz_rho_DM_allz, rho_DM_C) #outputs rho across all points
+
+        tmp_profs.kz_rho_DM_C = kz_rho_DM_allz[0]
+        tmp_profs.set_prof('kz_rho_DM_vec', kz_rho_DM_allz[1:], 0, gp)
+
+    tmp_rho_total_allz = tmp_rho_DM_allz*1.0 # Add DM to total mass density
 
     tmp_profs.rho_DM_C = tmp_rho_DM_allz[0]
     tmp_profs.set_prof('rho_DM_vec', tmp_rho_DM_allz[1:], 0, gp)
@@ -60,22 +66,19 @@ def geom_loglike(cube, ndim, nparams, gp):
     tmp_profs.rho_total_C = tmp_rho_total_allz[0]
     tmp_profs.set_prof('rho_total_vec', tmp_rho_total_allz[1:], 0, gp)
 
-    #DOING: print out tmp_profs DM, baryon, total
-    #print('loglike, rho_DM_Vec = ', tmp_profs.get_prof('rho_DM_vec', 0))
-    #print('loglike, rho_baryon_Vec = ', tmp_profs.get_prof('rho_baryon_vec', 0))
-    #print('loglike, rho_total_Vec = ', tmp_profs.get_prof('rho_total_vec', 0))
-
-
     #Tracer params, nu_C, kz_nu_C, kz_nu_vector
     for tracer_pop in range(0, gp.ntracer_pops):
         offstep = gp.nrhonu + 1
-        tracer_params = np.array(cube[off:off+offstep])
-        nu_C = tracer_params[0]
-        kz_nu_allz = tracer_params[1:] #kz for rho across all z points [0, bin_centres]
-        tmp_nu_allz = phys.rho(gp.z_all_pts, kz_nu_allz, nu_C) #outputs nu across all z points
+        if gp.scan_rhonu_space:
+            tmp_nu_allz = np.array(cube[off:off+offstep])
+        else:
+            tracer_params = np.array(cube[off:off+offstep])
+            nu_C = tracer_params[0]
+            kz_nu_allz = tracer_params[1:] #kz for rho across all z points [0, bin_centres]
+            tmp_nu_allz = phys.rho(gp.z_all_pts, kz_nu_allz, nu_C) #outputs nu across all z points
 
-        tmp_profs.kz_nu_C = kz_nu_allz[0]
-        tmp_profs.set_prof('kz_nu_vec', kz_nu_allz[1:], 0, gp)
+            tmp_profs.kz_nu_C = kz_nu_allz[0]
+            tmp_profs.set_prof('kz_nu_vec', kz_nu_allz[1:], 0, gp)
 
         tmp_profs.nu_C = tmp_nu_allz[0]
         tmp_profs.set_prof('nu_vec', tmp_nu_allz[1:], tracer_pop, gp)
@@ -100,13 +103,10 @@ def geom_loglike(cube, ndim, nparams, gp):
     tmp_profs.Sig_total_C = Sig_total_allz[0]
     tmp_profs.set_prof('Sig_total_vec', Sig_total_allz[1:], 0, gp)
 
-    Sig_DM_allz = phys.Sig(gp.z_all_pts, tmp_rho_total_allz) #SS not orrect zvec
-
-
     #Calculate sigma (velocity dispersion)
     #pdb.set_trace()
     try:
-        sigz2_vec = phys.sigz2(gp.z_all_pts, Sig_DM_allz, tmp_nu_allz, norm)
+        sigz2_vec = phys.sigz2(gp.z_all_pts, Sig_total_allz, tmp_nu_allz, norm)
     except ValueError:
         raise ValueError('negative value in sig2 array')
         return
@@ -116,7 +116,6 @@ def geom_loglike(cube, ndim, nparams, gp):
     chi2 = calc_chi2(tmp_profs, gp)
     gh.LOG(1, '   log L = ', -chi2/2.)
     tmp_profs.chi2 = chi2
-
 
     return tmp_profs   # from   likelihood L = exp(-\chi^2/2), want log of that
 ## \fn geom_loglike(cube, ndim, nparams, gp)

gravimage/programs/gl_mc_errors.py
@@ -27,7 +27,16 @@ def ErSamp_gauss_linear_w_z():
     return z_sampled
 
 
+z_data_flat_distro = rand.random(2000000)
+def ErSamp_flat_distro_test():
+    fraction_err = 0.001
+    z_data = z_data_flat_distro
 
+    z_sampled = []
+    for z_val in z_data:
+        z_sampled.append(abs(rand.normal(loc = z_val, scale = fraction_err)))
+
+    return z_sampled
 
 
 
@@ -37,16 +46,19 @@ def mc_nu_error(sampled_z_func, number_mcs, binmin, binmax, bincenter):
 
     nu_vectors=[]
     for jter in range(0, number_mcs):
-        print('jter = ', jter)
         jter_z_data = sampled_z_func()
         jter_nu, dummy, dummy, dummy, dummy = gh.nu_sig_from_bins(binmin, binmax, jter_z_data, np.ones(len(jter_z_data)))
         nu_vectors.append(jter_nu)
 
     #Calculate standard deviations of nu
     nu_vectors = np.array(nu_vectors)
-    nu_stdevs = []
+    nu_stdevs  = []
+    nu_means   = []
+    nu_medians = []
     for pter in range(0, len(binmin)):
         nu_stdevs.append(np.std(nu_vectors[:, pter]))
+        nu_means.append(np.mean(nu_vectors[:, pter]))
+        nu_medians.append(np.median(nu_vectors[:, pter]))
 
     #pdb.set_trace()
     #fig = plt.figure()
@@ -58,6 +70,9 @@ def mc_nu_error(sampled_z_func, number_mcs, binmin, binmax, bincenter):
 
 
 
-#if __name__=="__main__":
-#
-#    return
+if __name__=="__main__":
+
+    binmin, binmax, bincenter = gh.bin_r_linear(0.2, 0.8, 12)
+
+    nu_stdevs = mc_nu_error(ErSamp_flat_distro_test, 100, binmin, binmax, bincenter)
+    pdb.set_trace()